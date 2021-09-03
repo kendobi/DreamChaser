2983c7d207ec137c4c061475004721347035bb12
Kenny Kong <yokenny@gmail.com>
2021-09-03 15:44:45 -0700

    Mandalas Update

---
+++ Assets/FadeOverTime.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FadeOverTime : MonoBehaviour
+{
+    private Material mat;
+    private float opac;
+    private float t = 0.0f;
+    private float timeLimit = 1.0f;
+    public float speed = 0.1f;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        mat.SetFloat("_opacity", 0.0f);
+    }
+
+    void Update()
+    {
+        if (t < timeLimit)
+        {
+            // Animate the Shininess value
+            //float shininess = Mathf.PingPong(Time.time, 1.0f);
+            opac = Mathf.Lerp(0.0f, 1.0f, t);
+            mat.SetFloat("_opacity", opac);
+            t += speed * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,11 @@
+ using UnityEngine;
+ using System.Collections;
+ using UnityEngine.SceneManagement;
+ 
+ public class SceneLoader : MonoBehaviour {
+ 
+      public void LoadScene(int level)
+      { 
+         SceneManager.LoadScene(level);
+       }
+ }
