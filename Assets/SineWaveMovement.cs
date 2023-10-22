8e035598358a60bf9ae847246ed5203e69e831e4
Kenny Kong <yokenny@gmail.com>
2023-10-22 00:26:47 -0700

    updatezz

---
+++ Assets/SineWaveMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineWaveMovement : MonoBehaviour
+{
+    public float moveSpeed = 2.0f; // Speed at which the GameObject moves up
+    public float amplitude = 2.0f; // Amplitude of the sine wave
+    public float frequency = 1.0f; // Frequency of the sine wave
+
+    private float initialY;
+    private float time = 0.0f;
+
+    void Start()
+    {
+        initialY = transform.position.y;
+    }
+
+    void Update()
+    {
+        // Move the GameObject up
+        Vector3 newPosition = transform.position;
+        newPosition.y += moveSpeed * Time.deltaTime;
+        transform.position = newPosition;
+
+        // Create a sine wave motion in the x-direction
+        time += Time.deltaTime;
+        float xPosition = amplitude * Mathf.Sin(frequency * time);
+        newPosition.x = xPosition;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/SpiritEnergySpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritEnergySpawner : MonoBehaviour
+{
+    public GameObject spiritEnergyPrefab;
+    public Transform parentTransform;
+    public float spawnRate = 2.0f;  // Number of prefabs spawned per second
+    public float spawnAreaWidth = 200.0f;
+    public float spawnAreaHeight = 400.0f;
+    public float prefabLifespan = 5.0f; // Time in seconds before prefab is destroyed
+
+    private float spawnTimer = 0.0f;
+
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= 1.0f / spawnRate)
+        {
+            SpawnSpiritEnergy();
+            spawnTimer = 0.0f;
+        }
+    }
+
+    void SpawnSpiritEnergy()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(-spawnAreaWidth / 2, spawnAreaWidth / 2),
+            0,
+            Random.Range(-spawnAreaHeight / 2, spawnAreaHeight / 2)
+        );
+
+        GameObject spiritEnergy = Instantiate(spiritEnergyPrefab, spawnPosition, Quaternion.identity);
+
+        // Set the parent of the spawned object to the specified parentTransform
+        if (parentTransform != null)
+        {
+            spiritEnergy.transform.SetParent(parentTransform);
+        }
+
+        // Destroy the spawned prefab after the specified lifespan
+        Destroy(spiritEnergy, prefabLifespan);
+    }
+}
+
